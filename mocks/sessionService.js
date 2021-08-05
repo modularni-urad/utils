3b3be07d5a17d569f60dbc38d101dd2487111629
vencax <vencax@centrum.cz>
2021-08-05 15:11:05 +0200

    set mockUser route

--- mocks/sessionService.js
+++ mocks/sessionService.js
@@ -1,17 +1,21 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 
 export default function (port, g) {
   const app = express()
 
-  app.post('/sign', bodyParser.json(), (req, res) => {
+  app.post('/sign', express.json(), (req, res) => {
     g.sessionBasket.push([req.body, req.hostname])
     res.json({ token: 'beeep' })
   })
 
-  app.post('/verify', bodyParser.json(), (req, res) => {
+  app.post('/verify', express.json(), (req, res) => {
     g.sessionBasket.push([req.body, req.hostname])
     res.json(g.mockUser)
+  })
+
+  app.post('/set', express.json(), (req, res) => {
+    Object.assign(g.mockUser, req.body)
+    res.json({ message: 'ok' })
   })
 
   return app.listen(port)
