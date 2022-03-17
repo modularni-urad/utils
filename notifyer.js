eea0edbaf17a50bfb48fa8913b3f2d4e69d7a9c0
vencax <vencax@centrum.cz>
2022-03-17 10:59:32 +0100

    fix

--- notifyer.js
+++ notifyer.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import logger from './logger'
 
+const NOTIFYER_SERVICE = process.env.NOTIFYER_SERVICE || ''
+
 export function notifyUser (tenant, content, UID, type, data) {
   const payload = { content, UID, type, data }
-  const url = process.env.NOTIFYER_SERVICE.replace('{{TENANTID}}', tenant)
+  const url = NOTIFYER_SERVICE.replace('{{TENANTID}}', tenant)
   return axios.post(url, payload).catch(err => {
     logger.error(err)
   })
