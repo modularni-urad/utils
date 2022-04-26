1932fea2aba60882387f5a937be9a7f16a745996
vencax <vencax@centrum.cz>
2022-04-26 21:09:57 +0200

    LOG_500_ERRORS

--- error_handlers.js
+++ error_handlers.js
@@ -8,5 +8,6 @@ function generalErrorHlr (error, req, res, next) {
   if (error instanceof APIError) {
     return res.status(error.name).send(error.message)
   }
+  process.env.LOG_500_ERRORS && console.error(error)
   res.status(500).send(error.message || error.toString())
 }
